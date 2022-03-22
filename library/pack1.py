189256cc4f3aaa415c546af5c7fb079f78621b43
KarolinaM04 <kmatusiak2@st.swps.edu.pl>
2022-03-22 16:13:25 +0100

    MMM

---
+++ library/pack1.py
@@ -0,0 +1,5 @@
+def dodawanie (x, y):
+    x = int(input("Podaj cyfre:" ))
+    y = int(input("Podaj cyfre:" ))
+    math = (x + y)
+    return math
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='library',
+    version='1.0',
+    description='Moja biblioteka',
+    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    author='Karolina',
+    author_email='kmatusiak2@st.swps.edu.pl',
+    license='unlicense',
+    packages=['library'],
+    zip_safe=False
+)
